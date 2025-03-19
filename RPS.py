369b5ec47968b929c5ee43aefac1e2ddb5fed203
hasmikshahbazyan2004 <hasmikshahbazyan2004@gmail.com>
2025-03-19 22:18:53 +0400

    CHanges were made in the file

--- RPS.py
+++ RPS.py
@@ -1,1 +1,16 @@
-player_choice = "rock"
+import random
+
+def get_choice():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_win(player, computer): 
+    print("You chose " + player + ", computer chose: " + computer)
+    if player == computer:
+        return "It's a tie!"
+    
+
+
