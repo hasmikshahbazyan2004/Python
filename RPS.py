68e2655bb6ad3a280bccbdab0aadf289061f9a32
hasmikshahbazyan2004 <hasmikshahbazyan2004@gmail.com>
2025-03-20 18:29:50 +0400

    Finished rock-paper-scissors game

--- RPS.py
+++ RPS.py
@@ -8,9 +8,26 @@ def get_choice():
     return choices
 
 def check_win(player, computer): 
-    print("You chose " + player + ", computer chose: " + computer)
+    #print("You chose " + player + ", computer chose: " + computer)
+    print(f"You chose {player}, computer chose {computer}")
     if player == computer:
         return "It's a tie!"
-    
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "scissors":
+            return "Scissors cuts paper! You lose."
+        else:
+            return "Paper covers rock! You win!"
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
 
-
+choices = get_choice()
+result = check_win(choices["player"], choices["computer"])
+print(result)
